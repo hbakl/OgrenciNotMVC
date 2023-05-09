b3d9cda87c2ec2a7ce3c8946a4dddeab6e11a00a
hbakl <hbakl@DESKTOP-KSDVUIV>
2023-05-09 15:44:34 +0300

    Projeye başlanıldı. Veritabanı oluşturuldu. Veritabanı diyagramları ayarladı.

---
+++ OgrenciNotMVC/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMVC.Models.EntityFramework;
+
+
+namespace OgrenciNotMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+
+        public ActionResult Index()
+        {
+            var dersler = db.TBLDERSLER.ToList();
+            return View(dersler);
+        }
+    }
+}
